e73381af2a539c29585e5a2ba8b640b10b055523
William Cummings <william.cummings@conflictmanagerpro.com>
2022-09-06 05:39:17 -0400

    Updated astro config file do deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://willjr20.github.io',
+    base: '/astro-website',
+});
